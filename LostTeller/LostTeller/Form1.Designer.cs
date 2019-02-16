7a83fc16242a2ecf1c8b4b9fdcf6130b8a00c52d
Leo Wootsi <LeoWootsi@outlook.com>
2019-02-16 15:28:09 +0800

    Add files via upload

---
+++ LostTeller/LostTeller/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace LostTeller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ErrInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackColor = System.Drawing.Color.AliceBlue;
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 126);
+            this.panel1.TabIndex = 0;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // ErrInfo
+            // 
+            this.ErrInfo.BackColor = System.Drawing.Color.Azure;
+            this.ErrInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ErrInfo.CausesValidation = false;
+            this.ErrInfo.Enabled = false;
+            this.ErrInfo.Font = new System.Drawing.Font("宋体", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ErrInfo.Location = new System.Drawing.Point(13, 145);
+            this.ErrInfo.Name = "ErrInfo";
+            this.ErrInfo.ReadOnly = true;
+            this.ErrInfo.Size = new System.Drawing.Size(220, 13);
+            this.ErrInfo.TabIndex = 1;
+            this.ErrInfo.TabStop = false;
+            this.ErrInfo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 172);
+            this.Controls.Add(this.ErrInfo);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LostTeller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox ErrInfo;
+    }
+}
+
---
+++ LostTeller/LostTeller/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LostTeller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool Decode(string str)
+        {
+            if (Directory.Exists(str))
+            {
+                if (HasEncrypt(str))
+                {
+                    DirectoryInfo d = new DirectoryInfo(str);
+                    d.MoveTo(str.Substring(0, str.LastIndexOf('.')));                    
+                }
+                return true;
+            }
+            else return false;
+        }
+
+
+        private bool HasEncrypt(string str)
+        {
+            if (str.LastIndexOf(".{2559a1f2-21d7-11d4-bdaf-00c04f60b9f0}") == -1)
+                return false;
+            else return true;
+        }
+
+        private bool Encrypt(string str)
+        {
+            if (Directory.Exists(str))
+            {
+                DirectoryInfo d = new DirectoryInfo(str);
+                d.MoveTo(d.Parent.FullName + "\\" + d.Name + ".{2559a1f2-21d7-11d4-bdaf-00c04f60b9f0}");
+                return true;
+            }
+            else return false;
+        }
+
+        //单文件夹加密或解密
+        public bool SingleEncrypt(string path)
+        {
+            try
+            {
+                //如果没加密过
+                if (!HasEncrypt(path))
+                {
+                    //如果路径正确
+                    if (Encrypt(path))
+                    {
+                        return true;
+                    }
+                    else
+                        return false;
+                }
+                else//加密过则解密
+                {
+                    if (Decode(path))
+                    {
+                        return true;
+                    }
+                    else
+                        return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrInformLog(ex.Message);
+                return false;
+            }
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            Graphics grap = panel1.CreateGraphics();
+            Brush brush = null;
+            grap.Clear(Color.AliceBlue);
+            
+            string[] dirs = e.Data.GetData(DataFormats.FileDrop) as string[];
+            foreach (string s in dirs)
+            {
+                if(SingleEncrypt(s))
+                {
+                    brush = new SolidBrush(Color.Aqua);
+                    grap.DrawString("OK", new Font("楷体", 25), brush, panel1.Width / 2 - 25, panel1.Height / 2 - 20);
+                    ErrInformLog("OK");
+
+                }
+                else
+                {
+                    grap.Clear(Color.AliceBlue);
+                    brush = new SolidBrush(Color.OrangeRed);
+                    grap.DrawString("NO", new Font("楷体", 25), brush, panel1.Width / 2 - 25, panel1.Height / 2 - 20);
+                    ErrInformLog("Error!");
+                    break;
+                }
+            }
+            brush.Dispose();
+            grap.Dispose();
+        
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private delegate void ErrInformLogDelegate(string str);
+        private void ErrInformLog(string str)
+        {
+            if (ErrInfo.InvokeRequired)
+            {
+                ErrInformLogDelegate d = ErrInformLog;
+                ErrInfo.Invoke(d, str);
+            }
+            else
+            {
+                ErrInfo.Text = str;
+            }
+        }
+
+       
+    }
+}
